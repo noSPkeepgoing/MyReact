2a6bfab700e45f7ed510eebad1521a59c5d4185e
sunpaaaa <sunpaaaa@gmail.com>
2024-04-11 14:13:35 +0900

    복잡한 DOM 객체 간단한 DOM 객체로 만들어보기

---
+++ app.js
@@ -0,0 +1,50 @@
+function createDOM(node) {
+  // vdom이 문자열일 경우
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
+  const element = document.createElement(node.tag);
+
+  node.children.map(createDOM).forEach(element.appendChild.bind(element));
+
+  return element;
+}
+
+// 가상 돔을 만들어 보자!
+// HTML 구조 : 태그이름, 속성, 자식
+// 속성과 자식은 여러개가 가능 함 -> 객체 또는 배열 형식으로 지정
+// 속성은 key-value 쌍으로 구성 -> 객체 사용
+const vdom = {
+  tag: 'p',
+  props: {},
+  children: [
+    {
+      tag: 'h1',
+      props: {},
+      children: ['React 만들어보기'],
+    },
+    {
+      tag: 'ul',
+      props: {},
+      children: [
+        {
+          tag: 'li',
+          props: {},
+          children: ['첫 번째 아이템'],
+        },
+        {
+          tag: 'li',
+          props: {},
+          children: ['두 번째 아이템'],
+        },
+        {
+          tag: 'li',
+          props: {},
+          children: ['세 번째 아이템'],
+        },
+      ],
+    },
+  ],
+};
+
+document.querySelector('#root').appendChild(createDOM(vdom));
