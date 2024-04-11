96d0fa91bd706a74fa779c566f563cf9ec04b27a
sunpaaaa <sunpaaaa@gmail.com>
2024-04-11 15:53:42 +0900

    webpack과 babel을 이용해서 프로젝트 세팅해보기

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  devServer: {
+    compress: true,
+    port: 9999,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Object to DOM Render',
+      template: 'index.html',
+    }),
+  ],
+};
