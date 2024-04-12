962e5fdc9b0d2b34406f9962c48f5479e4ac0b8d
sunpaaaa <sunpaaaa@gmail.com>
2024-04-12 12:26:44 +0900

    babel 이용해서 DOM 객체 생성 함수 숨기기

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
-import { render, createElement } from './react';
+/** @jsx createElement */
+import { render, createElement } from './react'; // 런타임 오류 발생하기 위해 createElement 포함
 
 // 가상 돔을 만들어 보자!
 // HTML 구조 : 태그이름, 속성, 자식
 // 속성과 자식은 여러개가 가능 함 -> 객체 또는 배열 형식으로 지정
 // 속성은 key-value 쌍으로 구성 -> 객체 사용
 
-const vdom = createElement(
-  'p',
-  {},
-  createElement('h1', { style: 'color:coral' }, 'React 만들기!'),
-  createElement(
-    'ul',
-    {},
-    createElement('li', {}, '첫 번째 아이템'),
-    createElement('li', {}, '두 번째 아이템'),
-    createElement('li', {}, '세 번째 아이템')
-  )
+const vdom = (
+  <p>
+    <h1 style='color:tomato'>React 만들기!</h1>
+    <ul>
+      <li>첫 번째 아이템</li>
+      <li>두 번째 아이템</li>
+      <li>세 번째 아이템</li>
+    </ul>
+  </p>
 );
-
-console.log(vdom);
 
 render(vdom, document.querySelector('#root'));
