47331eaa0aebf27b924b1650b70df4f3ffd2503f
sunpaaaa <sunpaaaa@gmail.com>
2024-04-12 12:10:44 +0900

    DOM 객체 생성하는 함수 만들기

--- src/app.js
+++ src/app.js
@@ -1,40 +1,23 @@
-import { render } from './react';
+import { render, createElement } from './react';
 
 // 가상 돔을 만들어 보자!
 // HTML 구조 : 태그이름, 속성, 자식
 // 속성과 자식은 여러개가 가능 함 -> 객체 또는 배열 형식으로 지정
 // 속성은 key-value 쌍으로 구성 -> 객체 사용
-const vdom = {
-  tag: 'p',
-  props: {},
-  children: [
-    {
-      tag: 'h1',
-      props: { style: 'color:coral' },
-      children: ['React 만들어보기'],
-    },
-    {
-      tag: 'ul',
-      props: {},
-      children: [
-        {
-          tag: 'li',
-          props: {},
-          children: ['첫 번째 아이템'],
-        },
-        {
-          tag: 'li',
-          props: {},
-          children: ['두 번째 아이템'],
-        },
-        {
-          tag: 'li',
-          props: {},
-          children: ['세 번째 아이템'],
-        },
-      ],
-    },
-  ],
-};
+
+const vdom = createElement(
+  'p',
+  {},
+  createElement('h1', { style: 'color:coral' }, 'React 만들기!'),
+  createElement(
+    'ul',
+    {},
+    createElement('li', {}, '첫 번째 아이템'),
+    createElement('li', {}, '두 번째 아이템'),
+    createElement('li', {}, '세 번째 아이템')
+  )
+);
+
+console.log(vdom);
 
 render(vdom, document.querySelector('#root'));
--- src/react.js
+++ src/react.js
@@ -13,6 +13,10 @@ export function createDOM(node) {
   return element;
 }
 
+export function createElement(tag, props, ...children) {
+  return { tag, props, children };
+}
+
 export function render(vdom, container) {
   container.appendChild(createDOM(vdom));
 }
